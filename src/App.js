a593a876ba7574033c41dcd968f5808f802e6ae2
BShamblin <95258414+BShamblin@users.noreply.github.com>
2022-11-22 17:26:12 -0500

    Update App.js
    Added an import and Route for the homepage.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login/>}/>
+          <Route path="/Home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>

483ead69b7701c3fd2229c8e2786c987557436bd
huyle892 <89744549+huyle892@users.noreply.github.com>
2022-11-28 18:07:14 -0500

    Update propertyPage.jsx

--- src/Components/propertyPage.jsx
+++ src/Components/propertyPage.jsx
@@ -17,17 +17,17 @@ import {
   useNavigate,
   useParams
 } from "react-router-dom";
+import {useReactToPrint} from "react-to-print";
 
 function withRouter(Component) {
   function ComponentWithRouterProp(props) {
     let location = useLocation();
     let navigate = useNavigate();
     let params = useParams();
-    let name = useParams();
     return (
       <Component
         {...props}
-        router={{ location, navigate, params, name }}
+        router={{ location, navigate, params }}
       />
     );
   }
@@ -35,19 +35,78 @@ function withRouter(Component) {
   return ComponentWithRouterProp;
 }
 
- function PropertyPage(props) {
-   console.warn(props);
-  return (
-    <Box>
-    <h1 
+function PropertyPage(props) {
+  
+  console.warn(props);
+  const printComponent = React.useRef();
+  const Property_id = props.router.params.propertyId;
+  const [properties, setProperties] = React.useState([]);
+
+  const [currPid, setCurrPid] = React.useState([]);
+  
+  const accessToken = 'Bearer' + localStorage.getItem("jwtToken");
+
+  const handlePrint = useReactToPrint({
+    content: () => printComponent.current, 
+  });
+
+  React.useEffect(() => {
+    fetch("https://cen4010-pms-backend.herokuapp.com/api/cuser/getProperties", {
+        method: "GET",
+        headers: { 'Authorization': accessToken }
+    })
+        .then(res => res.json())
+        .then((result) => {
+            setProperties(result)
+        })
+        .catch(err => {
+            console.log(err)
+        });
+}, []);
+
+
+
+  async function editProperty(newPropertyName, newPropertyValue) {
+    const property = { name: newPropertyName, value: Number(newPropertyValue) }
+    const pidNum = Number(currPid)
+    console.log(property);
+    const response = await fetch(`https://cen4010-pms-backend.herokuapp.com/api/cuser/editProperty/${pidNum}`, {
+        method: "PUT",
+        headers: ({ "Content-Type": "application/json", 'Authorization': accessToken }),
+        body: JSON.stringify(property)
+    });
+    const data = await response.json();
+    console.log(data);
+}
+
+const [openEdit, setOpenEdit] = React.useState(false);
+    const handleClickEditOpen = (e) => {
+        setCurrPid(Property_id);
+        setOpenEdit(true);
+    };
+    const EditForm = React.useRef(null);
+    const handleEditClose = (e, pid) => {
+        if (e.target.id === "EditApplyBtn") {
+            const form = EditForm.current
+            const data = [form["propertyName"].value, form["propertyValue"].value]
+            console.log(data)
+            editProperty(form["propertyName"].value, form["propertyValue"].value, pid)
+        }
+        setOpenEdit(false);
+    };
+
+    return (
+    <>
+    <Box ref={printComponent}>
+    <h1
       style={{
         marginTop: 40,
         marginLeft: 35
       }}
-    >{props.router.params.propertyId}</h1>
+    >{Property_id}</h1>
     <Stack direction="row">
     
-    <Box 
+    <Box
       component="img"
       sx={{
         width: 0.55,
@@ -68,8 +127,9 @@ function withRouter(Component) {
           marginLeft: 3.5,
           marginTop: 3
         }}
+        
       >
-     
+
       </Typography>
     
     </Stack>
@@ -91,11 +151,82 @@ function withRouter(Component) {
         </Typography>
     </Stack>
         <Stack direction="row" sx={{marginTop: 3, marginLeft: 3.5}}>
-          <Button variant="contained" color="primary" style={{marginLeft: 3.5}}>Edit </Button>
+          <Button onClick={handleClickEditOpen}variant="contained" color="primary" style={{marginLeft: 3.5}}>Edit </Button>
           <Button variant="contained" color="warning" style={{marginLeft: 10}}>Delete</Button>
-          <Button variant="contained" color="primary" style={{marginLeft: 10}} endIcon={<PrintIcon/>}>Print</Button>
+          <Button onClick={handlePrint} variant="contained" color="primary" style={{marginLeft: 10}} endIcon={<PrintIcon/>}>Print</Button>
         </Stack>
     </Box>
+    
+    
+    <div id="Edit Property Dialog">
+      <Dialog 
+        open={openEdit} 
+        onClose={handleEditClose}
+        fullScreen
+      >
+        <AppBar sx={{ position: 'relative' }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleEditClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Button onClick={handleEditClose} variant="h6" component="div">
+              CANCEL
+            </Button>
+            <Button id="EditApplyBtn" onClick={handleEditClose} autoFocus color="inherit" >
+              SAVE
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <DialogTitle variant="h4">EDIT PROPERTY</DialogTitle>
+        <DialogContent>
+          <form ref={EditForm}>
+            <Grid>
+              <Grid item>
+                <Input
+                  autoFocus
+                  placeholder="Property Name"
+                  id="propertyName"
+                  label="Property Name"
+                  type="text"
+                  fullWidth
+                  variant="standard"
+                  sx={{marginTop: 2}}
+                />
+                <Input
+                  autoFocus
+                  placeholder="Property Value"
+                  id="propertyValue"
+                  label="Property Value"
+                  type="number"
+                  fullWidth
+                  multiline
+                  variant="standard"
+                  sx={{marginTop: 4}}
+                />
+              </Grid>
+              <Grid item>
+                <Button sx={{marginTop: 4}} variant="contained" component="label">
+                  Upload Image
+                  <input id="propetyImage" hidden accept="image/*" multiple type="file" />
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </div>
+
+
+
+    <div>
+
+    </div>
+    </>
   );
 }
 

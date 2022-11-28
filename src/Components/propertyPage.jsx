2539a50d642634bedfb443cfc964ef8ba2d15da5
huyle892 <89744549+huyle892@users.noreply.github.com>
2022-11-28 14:08:00 -0500

    Update propertyPage.jsx

--- src/Components/propertyPage.jsx
+++ src/Components/propertyPage.jsx
@@ -1,12 +1,42 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
-import { Typography } from '@mui/material';
+import { DialogContent, DialogTitle, Typography } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import PrintIcon from '@mui/icons-material/Print';
+import Dialog from '@mui/material/Dialog';
+import Grid from '@mui/material/Grid';
+import Input from '@mui/material/Input';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import {
+  useLocation,
+  useNavigate,
+  useParams
+} from "react-router-dom";
 
-export default function PropertyPage() {
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    let name = useParams();
+    return (
+      <Component
+        {...props}
+        router={{ location, navigate, params, name }}
+      />
+    );
+  }
+
+  return ComponentWithRouterProp;
+}
+
+ function PropertyPage(props) {
+   console.warn(props);
   return (
     <Box>
     <h1 
@@ -14,7 +44,7 @@ export default function PropertyPage() {
         marginTop: 40,
         marginLeft: 35
       }}
-    >PROPERTY NAME</h1>
+    >{props.router.params.propertyId}</h1>
     <Stack direction="row">
     
     <Box 
@@ -39,7 +69,7 @@ export default function PropertyPage() {
           marginTop: 3
         }}
       >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     
       </Typography>
     
     </Stack>
@@ -68,3 +98,5 @@ export default function PropertyPage() {
     </Box>
   );
 }
+
+export default withRouter(PropertyPage);

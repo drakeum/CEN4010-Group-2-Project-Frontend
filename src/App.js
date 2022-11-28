8d1717fbb037de79ca2dec9f662ba243b69384bc
huyle892 <89744549+huyle892@users.noreply.github.com>
2022-11-28 14:13:11 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Property_Page from "./pages/Property_Page";
+import {  
+  BrowserRouter as
+  Router,  
+  Routes,  
+  Route,  
+  Link  
+}   
+from 'react-router-dom';  
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+          <Routes>
+            <Route path="/" element={<Login/>} />
+            <Route exact path="/Home" element={<Home/>} /> 
+            <Route path="/Property_Page/:propertyId" element={<Property_Page />}/>
+          </Routes>
+      </Router>
     </div>
   );
 }

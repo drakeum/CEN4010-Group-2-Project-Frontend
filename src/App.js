e2844c255a6edc77bdc0e580d27f7953287026dc
huyle892 <89744549+huyle892@users.noreply.github.com>
2022-11-28 21:49:24 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import AdminHome from "./pages/AdminHome";
+import Property_Page from "./pages/Property_Page";
 function App() {
   return (
     <div>
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Login/>}/>
           <Route path="/Home" element={<Home/>}/>
           <Route path="/AdminHome" element={<AdminHome />} />
+          <Route path="/Property_Page/:propertyName/:propertyId" element={<Property_Page />}/>
         </Routes>
       </BrowserRouter>
     </div>

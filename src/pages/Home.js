4c2a811b4f2a9dcb17f42ef440b3418e4a46e404
BShamblin <95258414+BShamblin@users.noreply.github.com>
2022-11-28 21:17:17 -0500

    Update Home.js
    Updated for access to AdminHome.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,29 @@ import React from 'react'
 import Navbar from './Navbar';
 import UserProperties from './UserProperties';
 import UserProfile from './UserProfile';
+import { useNavigate } from "react-router-dom";
+import { useEffect } from 'react';
 
 function Home() {
+    
+     const accessToken = 'Bearer' + localStorage.getItem("jwtToken");
+
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        if (localStorage.getItem("jwtToken") === "undefined")
+            navigate('/')
+
+        fetch("https://cen4010-pms-backend.herokuapp.com/api/cuser/getRole", {
+            method: "GET",
+            headers: { 'Authorization': accessToken }
+        })
+            .then(res => res.json())
+            .then((result) => {
+                if (result[0] === "ROLE_ADMIN")
+                    navigate('/AdminHome')
+            });
+    }, []);
 
     function HomeDiv() {
 
